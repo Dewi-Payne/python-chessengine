a0ed268780c25f909b722efecd223df9f7f97796
Dewi-Payne <dewitbpayne@gmail.com>
2021-06-07 15:48:02 +0100

    Improved data flow, now pieces are stored in a list of squares with a class attribute piece instead of a dictionary

--- pychess.py
+++ pychess.py
@@ -3,18 +3,17 @@ import os
 import pathlib
 
 # Global variables
-# "pieces" maps squares to what piece occupies it
 FEN = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
-pieces = {}
-squares = {}
 move_from = None
 
 
 class Square:
     # class that represents a square
-    def __init__(self, col, row):
+    def __init__(self, col, row, canvas=None, piece=None):
         self.col = col
         self.row = row
+        self.canvas = canvas
+        self.piece = piece
 
 
 def index_to_square(index):
@@ -27,21 +26,37 @@ def index_to_square(index):
 class Board:
     # class that represents the board
     def __init__(self):
+        self.squares = []
+        self.initialise_squares()
+        self.read_pieces()
         self.draw()
+
+
+    def clear_pieces(self):
+        pass
+        # This caused problems elsewhere so i commented it out, it's being called where it shouldn't
+        #for element in self.squares:
+        #    element.piece = None
+
+    def initialise_squares(self):
+        for row in range(8):
+            for col in range(8):
+                self.squares.append(Square(col,row))
 
     def draw(self):
         # method for drawing the board based on the
         # position of pieces in the pieces dict
-        squares.clear()
+        self.clear_pieces()
         for row in range(8):
             for col in range(8):
                 # Initialises the piece string to empty
                 # before checking it it is occupied by a piece
                 # This part could be improved
                 piece = ""
-                for i in pieces.keys():
-                    if (i.col == col) and (i.row == row):
-                        piece = pieces[i].colour[0] + pieces[i].piece_type + ".png"
+                for square in self.squares:
+                    if (square.col == col) and (square.row == row):
+                        if square.piece != None:
+                            piece = square.piece.colour[0] + square.piece.piece_type + ".png"
 
                 # determines the colour of each square
                 if (row + col) % 2 == 0:
@@ -54,16 +69,50 @@ class Board:
                 temp = tk.Canvas(root, width=50, height=50, bg=colour, bd=0,
                                  highlightthickness=0, relief='ridge')
 
-                squares[temp] = Square(col, row)
+                #squares[temp] = Square(col, row)
+                for element in self.squares:
+                    if element.row == row and element.col == col:
+                        element.canvas = temp
+                        # This binds the canvas to a function used to make moves
+                        temp.bind("<Button-1>", lambda e, s=element: square_clicked(e, s))
+                        temp.bind("<Button-3>", lambda e: clear_move())
 
                 if piece != "":
-                    temp.create_image(25, 25, image=images[piece])
-
-                # This binds the canvas to a function used to make moves
-                temp.bind("<Button-1>", lambda e, t=temp: square_clicked(e, t))
-                temp.bind("<Button-3>", lambda e: clear_move())
+                    temp.create_image(24, 25, image=images[piece])
 
                 temp.grid(row=row, column=col)
+
+    def read_pieces(self):
+        # This function reads a FEN string and places them in
+        # the pieces dict as values with its square as their key
+        # TODO - make it take in the FEN string as an argument that can be passed to it on moving
+        #pieces.clear()
+        self.clear_pieces()
+        col = 0
+        row = 0
+        for char in FEN:
+            if (col >= 9) or (char == "/"):
+                col = 0
+                row += 1
+                continue
+            if char == " ":
+                break
+            if char.isdigit():
+                col += int(char)
+            else:
+                #squ = Square(col, row)
+                piece_type = char
+
+                if char.isupper():
+                    colour = "white"
+                else:
+                    colour = "black"
+                #pieces[squ] = Piece(colour, piece_type)
+                for element in self.squares:
+                    if element.row == row and element.col == col:
+                        element.piece = Piece(colour, piece_type)
+
+                col += 1
 
 
 class Piece:
@@ -79,36 +128,43 @@ class Move:
         self.square_from = square_from
         self.square_to = square_to
 
-        try:
-            # This is a lazy way of doing it, maybe a method in
-            # the square class could do this better
-            for square in list(pieces.keys()):
-                if square.col == square_from.col and square.row == square_from.row:
-                    square_from = square
-                if square.col == square_to.col and square.row == square_to.row:
-                    square_to = square
 
-            #
-            pieces[square_to] = pieces[square_from]
-            pieces.pop(square_from)
+        # This is a lazy way of doing it, maybe a method in
+        # the square class could do this better
+        for square in list(board.squares):
+            if square.col == square_from.col and square.row == square_from.row:
+                square_from = square
+            if square.col == square_to.col and square.row == square_to.row:
+                square_to = square
 
-        except:
-            clear_move()
+        # This is some basic move legality checks, should be its own function probably
+        if square_from.piece is None:
+            board.draw()
+            return
+        if square_to.piece is not None:
+            if square_from.piece.colour == square_to.piece.colour:
+                board.draw()
+                return
+
+
+        square_to.piece = square_from.piece
+        square_from.piece = None
+
 
         board.draw()
 
 
-def square_clicked(event, obj):
-    obj.config(bg="red")
+def square_clicked(event, square):
+    square.canvas.config(bg="red")
     global move_from
     if move_from is None:
-        move_from = obj
+        move_from = square
     else:
-        if move_from == obj:
+        if move_from == square:
             move_from = None
             board.draw()
             return
-        move = Move(squares[move_from], squares[obj])
+        move = Move(move_from, square)
         move_from = None
 
 
@@ -117,33 +173,6 @@ def clear_move():
     global move_from
     move_from = None
 
-
-def read_pieces():
-    # This function reads a FEN string and places them in
-    # the pieces dict as values with its square as their key
-    # TODO - make it take in the FEN string as an argument that can be passed to it on moving
-    pieces.clear()
-    col = 0
-    row = 0
-    for char in FEN:
-        if (col >= 9) or (char == "/"):
-            col = 0
-            row += 1
-            continue
-        if char == " ":
-            break
-        if char.isdigit():
-            col += int(char)
-        else:
-            squ = Square(col, row)
-            piece_type = char
-
-            if char.isupper():
-                colour = "white"
-            else:
-                colour = "black"
-            pieces[squ] = Piece(colour, piece_type)
-            col += 1
 
 
 if __name__ == "__main__":
@@ -155,12 +184,6 @@ if __name__ == "__main__":
     image_dir = os.path.join(local_dir, "images")
     for filename in os.listdir(image_dir):
         images[filename] = tk.PhotoImage(file=image_dir + "/" + filename)
-    print(images)
-
-    read_pieces()
-
-    for p in pieces:
-        print(p.col, p.row, pieces[p].colour, pieces[p].piece_type)
 
     board = Board()
 
